adc1b4f34a7e47ce356dbd0242019bfc5be159d9
shantanu44444444 <76909672+shantanu44444444@users.noreply.github.com>
2021-06-18 17:45:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(440 , 440)
+    canvas.center();
+}
+
+function draw (){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
